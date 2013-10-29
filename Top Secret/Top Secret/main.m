650cf22096503262434198af1f3ce11cc46129c1
Eric Welsh <Eric_M_Welsh@hotmail.com>
2013-10-29 16:57:00 -0500

    Initial Project
    Initial Project Creation

---
+++ Top Secret/Top Secret/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Top Secret
+//
+//  Created by Eric Welsh on 10/29/13.
+//  Copyright (c) 2013 Bradley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Top Secret/Top SecretTests/Top_SecretTests.m
@@ -0,0 +1,34 @@
+//
+//  Top_SecretTests.m
+//  Top SecretTests
+//
+//  Created by Eric Welsh on 10/29/13.
+//  Copyright (c) 2013 Bradley. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Top_SecretTests : XCTestCase
+
+@end
+
+@implementation Top_SecretTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
